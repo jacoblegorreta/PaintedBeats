b150bda29c465df592b2ec52ea8186c5b5943bcc
Jacob Legorreta <jacobslegorreta@gmail.com>
2020-06-25 15:54:03 -0700

    ui design improvements

--- app/routes/application.js
+++ app/routes/application.js
@@ -13,10 +13,10 @@ export default class ApplicationRoute extends Route {
         id: 1, name : 'Beat Example', tempo:   90
       });
 
-      beat.createChannel([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 'hihat_closed');
-      beat.createChannel([0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1], 'clap');
-      beat.createChannel([1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1], 'kick');
-      beat.createChannel([1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0], 'cowbell');
-      beat.createChannel([0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1], 'maracas');
+      beat.createChannel([1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 'hihat_Cl');
+      beat.createChannel([0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], 'clap');
+      beat.createChannel([1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1], 'kick');
+  //    beat.createChannel([1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0], 'cowbell');
+  //    beat.createChannel([0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1], 'maracas');
     }
 }
